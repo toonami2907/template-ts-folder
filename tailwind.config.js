de43855b5de908051f0fb63f27d6d5d0c8ece456
Effiong Michael Ikem <115380518+toonami2907@users.noreply.github.com>
2024-10-28 03:51:58 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Include both .js and .ts/.tsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
